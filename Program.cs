914858a42198661f728cbef7f2f18ac2b59ee09f
Pedro Melo <pedro2002melo@gmail.com>
2023-12-14 18:05:16 +0000

    Almost Done

--- Program.cs
+++ Program.cs
@@ -5,44 +5,32 @@
         static void Main(string[] args)
         {
             string sourceDir = @"C:\Users\pedro\Desktop\tudo\teste";
-            string destinationDir = @"C:\Users\pedro\Desktop\tudo\test6";
+            string destinationDir = @"C:\Users\pedro\Desktop\tudo\test7";
 
-            try
+            //Sync 
+            int intervalo = 1 * 60 * 1000;
+
+            //Create a timer
+            Timer timer = new Timer(SyncDirectories, new { SourceDir = sourceDir, DestinationDir = destinationDir }, 0, intervalo);
+
+
+
+
+         
+
+            Console.WriteLine("Sync satrated. Press Q to quit");
+            while (Console.ReadKey(true).Key != ConsoleKey.Q)
             {
-                // Check if the source directory exists
-                if (Directory.Exists(sourceDir))
-                {
 
-                    if (!Directory.Exists(destinationDir))
-                    {
-                        Directory.CreateDirectory(destinationDir);
-                    }
-
-                    // Get all files in the source directory
-                    string[] files = Directory.GetFiles(sourceDir);
-
-                    // Copy each file to the destination directory
-                    foreach (string file in files)
-                    {
-                        string fileName = Path.GetFileName(file);
-                        string destFile = Path.Combine(destinationDir, fileName);
-                        File.Copy(file, destFile, true); // 'true' to overwrite existing files if necessary
-                    }
-
-                    Console.WriteLine("Directory duplicated successfully.");
-                }
-                else
-                {
-                    Console.WriteLine("Source directory does not exist.");
-                }
             }
-            catch (Exception e)
+            timer.Dispose();
+
+            Console.WriteLine("Press F to clear the directory");
+            while (Console.ReadKey(true).Key != ConsoleKey.F)
             {
-                Console.WriteLine($"Error: {e.Message}");
+
             }
 
-            Console.WriteLine("Carregue em uma tecla para limpar o diretorio replicado");
-            Console.ReadKey();
             if (Directory.Exists(destinationDir))
             {
                 // Check if the destination directory exists
@@ -62,8 +50,59 @@
                 }
 
             }
+            Console.WriteLine("Folder Clear");
 
+           
+        }
+        static void SyncDirectories(object state)
+        {
+            dynamic directories = state;
+            string sourceDir = directories.SourceDir;
+            string destinationDir = directories.DestinationDir;
 
+            try
+            {
+                // Check if the source directory exists
+                if (Directory.Exists(sourceDir))
+                {
+                    // Check if the destination directory exists
+                    if (Directory.Exists(destinationDir))
+                    {
+                        // Delete all files in the destination directory
+                        string[] destFiles = Directory.GetFiles(destinationDir);
+                        foreach (string file in destFiles)
+                        {
+                            File.Delete(file);
+                        }
+                    }
+                    else
+                    {
+                        // Create the destination directory if it doesn't exist
+                        Directory.CreateDirectory(destinationDir);
+                    }
+
+                    // Get all files in the source directory
+                    string[] files = Directory.GetFiles(sourceDir);
+
+                    // Copy each file to the destination directory
+                    foreach (string file in files)
+                    {
+                        string fileName = Path.GetFileName(file);
+                        string destFile = Path.Combine(destinationDir, fileName);
+                        File.Copy(file, destFile, true); // 'true' to overwrite existing files if necessary
+                    }
+
+                    Console.WriteLine($"Synchronized directories at {DateTime.Now}.");
+                }
+                else
+                {
+                    Console.WriteLine("Source directory does not exist.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
         }
     }
 }
